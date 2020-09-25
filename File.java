b8e01e33bd4400cd608d077b6908bdee5163a1f5
Dee121-ala <deekshitaalavala@gmail.com>
2020-09-25 16:55:13 +0530

    Usecase 5

--- File.java
+++ File.java
@@ -2,14 +2,19 @@ public class File
 {
 public static final int full_time=1;
 public static final int part_time=2;
+public static final int maxdays=20;
 public static final int rate=20;
 public static void main(String args[])
 {
 int emphrs=0;
 int empwage=0;
-double check=Math.floor(Math.random()*10 % 3);
+int totalwage=0;
 System.out.println("Welcome to Employee Wage");
+for(int i=0;i<maxdays;i++)
+{
+int check=(int)Math.floor(Math.random()*10 % 3);
 switch(check)
+{
 case full_time:
 emphrs=8;
 break;
@@ -18,7 +23,12 @@ emphrs=4;
 break;
 default:
 emphrs=0;
+}
 empwage = emphrs * rate;
+totalwage +=empwage;
 System.out.println("Employee wage is "+empwage);
+
+}
+System.out.println("Total employee wage is "+totalwage);
 }
 }
