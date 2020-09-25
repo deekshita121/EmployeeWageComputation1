1d4c09d18172038f5e68b09a3d5f53ea046bc596
Dee121-ala <deekshitaalavala@gmail.com>
2020-09-25 16:11:07 +0530

    Usecase 3

--- File.java
+++ File.java
@@ -1,6 +1,7 @@
 public class File
 {
-public static final int present=1;
+public static final int full_time=1;
+public static final int part_time=2;
 public static final int rate=20;
 public static void main(String args[])
 {
@@ -8,9 +9,13 @@ int emphrs=0;
 int empwage=0;
 double check=Math.floor(Math.random()*10 % 3);
 System.out.println("Welcome to Employee Wage");
-if(check==present)
+if(check==full_time)
 {
 emphrs=8;
+}
+else if(check==part_time)
+{
+emphrs=4;
 }
 else
 {
